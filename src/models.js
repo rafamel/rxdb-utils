35dfcb98c4e1b5a1e5f517fafec3de54e699ef4d
rafamel <melrcm@gmx.net>
2018-09-11 16:55:17 +0200

    Fix models

--- src/models.js
+++ src/models.js
@@ -4,8 +4,13 @@ export default {
     RxDatabase(proto) {
       Object.assign(proto, {
         models(models) {
-          if (!Array.isArray(models)) models = Object.values(models);
-          if (!models.length) return Promise.resolve(this);
+          if (Array.isArray(models)) {
+            if (!models.length) return Promise.resolve(this);
+          } else {
+            if (!models) return Promise.resolve(this);
+            else models = [models];
+          }
+
           const collections = models.map((model) => this.collection(model));
           return Promise.all(collections).then(() => this);
         }
