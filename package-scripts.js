d43d03f8a94413526fe785f86ab7b7bc196e4ef4
Rafa Mel <melrcm@gmx.net>
2018-09-29 06:03:35 +0200

    Boilerplate update

--- package-scripts.js
+++ package-scripts.js
@@ -8,19 +8,20 @@ const exit0 = (x) => `${x} || shx echo `;
 const series = (x) => `(${x.join(') && (')})`;
 // const intrim = (x) => x.replace(/\n/g, ' ').replace(/ {2,}/g, ' ');
 
+const OUT_DIR = 'build';
+
 process.env.LOG_LEVEL = 'disable';
 module.exports = scripts({
   build: series([
     'nps validate',
-    exit0('shx rm -r lib'),
-    'shx mkdir lib',
-    'babel src --out-dir lib',
-    'shx cp LICENSE ./lib',
-    sx('package')
+    exit0(`shx rm -r ${OUT_DIR}`),
+    `shx mkdir ${OUT_DIR}`,
+    sx('package'),
+    `babel src --out-dir ${OUT_DIR}`
   ]),
-  publish: 'nps build && cd ./lib && npm publish',
+  publish: `nps build && cd ${OUT_DIR} && npm publish`,
   watch: 'onchange "./src/**/*.{js,jsx,ts}" -i -- nps private.watch',
-  fix: `prettier --write "./**/*.{js,jsx,ts,scss}"`,
+  fix: `prettier --write "./**/*.{js,jsx,ts,json,scss}"`,
   lint: {
     default: 'eslint ./src --ext .js',
     test: 'eslint ./test --ext .js',
@@ -33,10 +34,10 @@ module.exports = scripts({
   },
   validate: 'nps fix lint lint.test lint.md test private.validate_last',
   update: 'npm update --save/save-dev && npm outdated',
-  clean: `${exit0('shx rm -r lib coverage')} && shx rm -rf node_modules`,
+  clean: `${exit0(`shx rm -r ${OUT_DIR} coverage`)} && shx rm -rf node_modules`,
   // Private
   private: {
-    watch: `${sx('clear')} && nps lint && babel src --out-dir lib`,
+    watch: `${sx('clear')} && nps lint && babel src --out-dir ${OUT_DIR}`,
     test_watch: `${sx('clear')} && nps test`,
     validate_last: `npm outdated || ${sx('countdown')}`
   }
@@ -53,6 +54,17 @@ registerSx({
     }, 1000);
   },
   package: () => {
+    // Copy all files
+    fs.readdirSync(__dirname)
+      .filter((x) => !fs.lstatSync(path.join(__dirname, x)).isDirectory())
+      .forEach((x) => {
+        if (x === 'package.json') return;
+        fs.createReadStream(path.join(__dirname, x)).pipe(
+          fs.createWriteStream(path.join(__dirname, OUT_DIR, x))
+        );
+      });
+
+    // Modify package.json
     const plain = fs.readFileSync(path.join(__dirname, 'package.json'));
     const package = JSON.parse(plain);
 
@@ -61,7 +73,7 @@ registerSx({
     delete package.scripts.publish;
 
     fs.writeFileSync(
-      path.join(__dirname, 'lib/package.json'),
+      path.join(__dirname, OUT_DIR, 'package.json'),
       JSON.stringify(package, null, 2)
     );
   }
